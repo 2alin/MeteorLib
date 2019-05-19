3fe17fe8712a0a891fbb0e7749ffd64056243fa3
2alin <adilsonsdev@gmail.com>
2019-05-19 05:29:53 -0500

    add action types

---
+++ src/actionTypes/index.tsx
@@ -0,0 +1,9 @@
+export const UPDATE_LIST = 'UPDATE_LIST';
+
+export const UPDATE_OPTIONS = 'UPDATE_OPTIONS';
+
+export const CLEAR_OPTIONS = 'CLEAR_OPTIONS';
+
+export const NEXT_PAGE = 'NEXT_PAGE';
+
+export const SET_LANGUAGE = 'SET_LANGUAGE';
